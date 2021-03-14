ae024c3dbbd4c7dc44943e1319e8d3507c08739d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-14 12:57:14 +0100

    setup jest

---
+++ jest.setup.js
@@ -0,0 +1,22 @@
+import 'jest-enzyme'
+
+import renderer from 'react-test-renderer'
+
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+import 'expect-puppeteer'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+global.renderToJSON = (component) => renderer.create(component).toJSON()
+
+global.renderMount = (component) => mount(component)
+
+global.renderShallow = (component) => shallow(component)
+
+require('whatwg-fetch')
+
+require('expect-puppeteer')
+
+jest.setTimeout(120e3)
