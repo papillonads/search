005d06b2abc067857550685e279a3f8997f08abd
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-14 21:02:02 +0100

    remove reducer listCars update edit

--- packages/core/src/store/reducer/ui/listCars/update/carsObjects.js
+++
@@ -1,5 +0,0 @@
-export function updateCarsObjects({ carsObjects, object }) {
-  return carsObjects?.map((carsObject) =>
-    carsObject.id === object.id ? { ...object, modified: new Date(), isSelected: false } : { ...carsObject, isSelected: false },
-  )
-}
