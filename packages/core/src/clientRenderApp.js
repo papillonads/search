88223b1335d51b4212626c51472e9cfb77b71d85
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-14 13:02:15 +0100

    add core index and client render app

---
+++ packages/core/src/clientRenderApp.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { primer } from '@papillonads/components'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { withRouter } from 'react-router'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { checkAccessibilityIssues } from '@papillonads/library/a11y'
+import { startPagePath } from './route/path'
+import { appRootRoute, listCarPageRoute, notFoundPageRoute } from './route'
+import { clientAppStore } from './store/client'
+
+/* istanbul ignore next */
+export function App() {
+  const { ErrorBoundary } = primer
+
+  return (
+    <ErrorBoundary>
+      <Provider store={clientAppStore}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact={appRootRoute.exact} path={appRootRoute.path}>
+              <Redirect to={startPagePath} />
+            </Route>
+            <Route exact={listCarPageRoute.exact} path={listCarPageRoute.path} component={listCarPageRoute.clientComponent} />
+            <Route component={withRouter(notFoundPageRoute.clientComponent)} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
+  )
+}
+
+/* istanbul ignore next */
+function renderApp() {
+  checkAccessibilityIssues(React, ReactDOM, 1000)
+
+  ReactDOM.hydrate(<App />, document.getElementById('app'))
+
+  if (module.hot) {
+    module.hot.accept()
+  }
+}
+
+renderApp()
