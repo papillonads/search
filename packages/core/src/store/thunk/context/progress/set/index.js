168d3d2eb4b39a2d17e7af1730c453948be1c834
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-05-17 11:21:36 +0200

    bumpstack search

--- packages/core/src/store/thunk/context/progress/set/index.js
+++ packages/core/src/store/thunk/context/progress/set/index.js
@@ -3,27 +3,29 @@ import * as actionCreators from '../../../../action/actionCreators'
 import { timeout as timeoutDefault, messageType } from '../../../../../library/constant'
 import { alertTextApp } from '../../../../../library/constant/alertText/app'
 
-export const contextSetProgressRegularThunk = ({ message: { text, type }, isLoading = false, timeout = timeoutDefault.alert }) => async (
-  dispatch,
-) => {
-  const { contextSetProgressAction } = bindActionCreators(actionCreators, dispatch)
+export const contextSetProgressRegularThunk =
+  ({ message: { text, type }, isLoading = false, timeout = timeoutDefault.alert }) =>
+  async (dispatch) => {
+    const { contextSetProgressAction } = bindActionCreators(actionCreators, dispatch)
 
-  contextSetProgressAction({ message: { text, type }, isLoading })
+    contextSetProgressAction({ message: { text, type }, isLoading })
 
-  if (type !== messageType.error) {
-    setTimeout(() => {
-      contextSetProgressAction({ message: { text: alertTextApp.ready, type: messageType.success }, isLoading: false })
-    }, timeout)
+    if (type !== messageType.error) {
+      setTimeout(() => {
+        contextSetProgressAction({ message: { text: alertTextApp.ready, type: messageType.success }, isLoading: false })
+      }, timeout)
+    }
   }
-}
 
-export const contextSetProgressConsentThunk = ({ message: { text, type }, isLoading, consent }) => async (dispatch) => {
-  const { contextSetProgressAction } = bindActionCreators(actionCreators, dispatch)
+export const contextSetProgressConsentThunk =
+  ({ message: { text, type }, isLoading, consent }) =>
+  async (dispatch) => {
+    const { contextSetProgressAction } = bindActionCreators(actionCreators, dispatch)
 
-  const {
-    action: { approve, cancel },
-    withInput,
-  } = consent
+    const {
+      action: { approve, cancel },
+      withInput,
+    } = consent
 
-  contextSetProgressAction({ message: { text, type }, isLoading, consent: { action: { approve, cancel }, withInput } })
-}
+    contextSetProgressAction({ message: { text, type }, isLoading, consent: { action: { approve, cancel }, withInput } })
+  }
