2e5622cc0698474ceae4231b211cb1093ccc3e2d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-02-02 15:06:47 +0100

    bumpstack search comply with node v14.19.0

--- packages/core/webpack.common.js
+++ packages/core/webpack.common.js
@@ -34,7 +34,7 @@ const configureBabelLoader = (browserList) => {
             {
               modules: false,
               useBuiltIns: 'entry',
-              corejs: '3.20.2',
+              corejs: '3.21.0',
               targets: {
                 browsers: browserList,
               },
